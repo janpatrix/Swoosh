8573d73e151bd34d383c080ce1332fbabc596f60
janpatrix <janpatrix@gmail.com>
2019-02-10 22:12:11 +0100

    added difficulty layout

---
+++ app/src/main/java/com/example/swoosh/DifficultyActivity.kt
@@ -0,0 +1,15 @@
+package com.example.swoosh
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_difficulty.*
+
+class DifficultyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_difficulty)
+    }
+}
--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,12 +1,20 @@
 package com.example.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        leagueNextBtn.setOnClickListener {
+
+            val difficultyInten = Intent(this, DifficultyActivity::class.java)
+            startActivity(difficultyInten)
+        }
     }
 }
